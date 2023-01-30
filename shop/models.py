1976e6126a66cc4e46ce8350e25e9d04f4575b27
Sobir Alimov <sobiriddin@gmail.com>
2023-01-30 07:38:05 +0500

    created customers model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,22 @@
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
+class  Customers(models.Model):
+    name = models.CharField("Customer",max_length=30)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=50)
+    adress = models.CharField(max_length=300)
+    contact = models.CharField(max_length=200)
+
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Customer'
+        verbose_name_plural = 'Customers'
